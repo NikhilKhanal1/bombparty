1c7e53d18fb05807b4028d46e6b6d0453ffacef6
Nikhilkhanal1 <nikhilkhanal@gmail.com>
2026-06-08 22:41:58 -0700

    slice 2: socket.io wired up, connection logging works

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
